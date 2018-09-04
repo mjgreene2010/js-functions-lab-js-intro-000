9bb57f6540a7fdd2600fe36765f2b32121ee1ebc
mjgreene2010@gmail.com <mjgreene2010@gmail.com>
2018-09-04 21:41:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function happyHolidayTo(holiday, name){
 }
 
 function holidayCountdown(holiday, days){
-  return `It's ${days} days unit ${holiday}!`
+  return `It's ${days} days until ${holiday}!`
   
 }
